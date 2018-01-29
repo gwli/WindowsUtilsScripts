1859b833a1cc60ae56cae16c59b829ec8e37b137
victor <gwli@outlook.com>
2018-01-29 00:36:49 -0800

    check all VS and MSBuild

--- Uninstall_check_VS/uninstall_check_VS_py.py
+++ Uninstall_check_VS/uninstall_check_VS_py.py
@@ -1,21 +1,38 @@
 import os
+# 3.5+ support iglob ** recursive
 import glob
 import shutil
-vs2013="C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/Extensions"
-vs2015="C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/Extensions"
-vs2017="C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/Common7/IDE/Extensions"
+
+
+vspath_lists = [
+              ("VS2013","C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/Extensions/NVIDIA/Nsight Tegra"),
+              ("VS2015","C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/Extensions/NVIDIA/Nsight Tegra"),
+              ("VS2017","C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/Common7/IDE/Extensions"),
+              ("MSBuild_2013","C:/Program Files (x86)/MSBuild/Microsoft.Cpp/v4.0/v120/Platforms"),
+              ("MSBuild_2015","C:/Program Files (x86)/MSBuild/Microsoft.Cpp/v4.0/v140/Platforms"),
+              ("MSBuild_2017","C:/Program Files (x86)/Microsoft VisualStudio/2017/Enterprise/Common7/IDE/VC/VCTargets/Platforms"),
+           ]
+
 
 def get_pentaK_path(vspath):
-    extension_list = os.listdir(vspath)
-    pentak_dir = [ i for i in extension_list if glob.glob(os.path.join(vspath,i,"Nvidia.PentaK*"))]
-    print(vspath,":",pentak_dir)
-    return [os.path.join(vspath,i) for i in pentak_dir]
+    branch_name = vspath[0]
+    install_dir = vspath[1]
+    if not os.path.exists(install_dir):
+         return None
+    
+    extension_list = os.listdir(install_dir)
+    def find_pentaK(search_path):
+        patterns = [ os.path.join(install_dir,search_path,i) for i in ("**\\Nvidia.PentaK*", "Nvidia.PentaK*","*Android-NVIDIA*")]
+        #print(patterns)
+        return any([glob.glob(pat,recursive=True) for pat in patterns])
+
+    pentak_dir = [ i for i in extension_list if find_pentaK(i)]
+    print(install_dir,":",pentak_dir)
+    return [os.path.join(install_dir,i) for i in pentak_dir]
 
 def batch_check():
-    global vs2013
-    global vs2015
-    global vs2017
-    print([get_pentaK_path(i) for i in (vs2013,vs2015,vs2017)])
+    global vspath_lists
+    print([get_pentaK_path(i) for i in vspath_lists])
 
 def cleanup(cleandir):
     for i in cleandir:
@@ -23,10 +40,8 @@ def cleanup(cleandir):
         shutil.rmtree(i)
 
 def batch_clean():
-    global vs2013
-    global vs2015
-    global vs2017
-    for j in [get_pentaK_path(i) for i in (vs2013,vs2015,vs2017)]:
+    global vspath_lists
+    for j in [get_pentaK_path(i) for i in vspath_lists]:
         cleanup(j)
 
 
